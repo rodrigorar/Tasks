86c373f30f5aff379a6c895b02d7b55466fcf47f
rodrigorar <rodrigoramos.rosa2@gmail.com>
2017-07-14 01:40:11 +0100

    Initial Commit

---
+++ src/main/java/rodrigorar/App.java
@@ -0,0 +1,13 @@
+package rodrigorar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
