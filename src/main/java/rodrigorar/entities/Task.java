4fb486eb8fda27379afb792a279976cd3e29336b
rodrigorar <rodrigoramos.rosa2@gmail.com>
2017-07-17 21:45:40 +0100

    Task entity

---
+++ src/main/java/rodrigorar/entities/Task.java
@@ -0,0 +1,50 @@
+package rodrigorar.entities;
+
+import rodrigorar.utils.IdGenerator;
+import rodrigorar.entities.exceptions.InvalidTitleException;
+
+public class Task {
+    private String _id;
+    private String _title;
+    private String _description;
+
+    public Task(String title)
+    throws
+    InvalidTitleException {
+        this.setTitle(title);
+        _id = IdGenerator.getInstance().generateTaskId();
+    }
+
+    public Task(String title, String description)
+    throws
+    InvalidTitleException {
+        this(title);
+        this.setDescription(description);
+    }
+
+    public String getId() {
+        return _id;
+    }
+
+    public void setTitle(String title)
+    throws
+    InvalidTitleException {
+        if (title != null && !title.equals("")) {
+            _title = title;
+        } else {
+            throw new InvalidTitleException("The title is invalid.");
+        }
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    public void setDescription(String description) {
+        _description = description;
+    }
+
+    public String getDescription() {
+        return _description;
+    }
+}
---
+++ src/main/java/rodrigorar/entities/exceptions/InvalidTitleException.java
@@ -0,0 +1,9 @@
+package rodrigorar.entities.exceptions;
+
+public class InvalidTitleException
+extends Exception {
+
+    public InvalidTitleException(String message) {
+        super(message);
+    }
+}
