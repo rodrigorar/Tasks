63c15a31aa82904eeec2fc661cd4b0ee77c8aa5e
rodrigorar <rodrigoramos.rosa2@gmail.com>
2017-07-20 22:06:35 +0100

    Labels

---
+++ src/main/java/rodrigorar/ui/Labels.java
@@ -0,0 +1,11 @@
+package rodrigorar.ui;
+
+public final class Labels {
+    public static final String SAVE = "Save";
+    public static final String EDIT = "Edit";
+    public static final String DESCRIPTION = "Description";
+    public static final String TITLE = "Title";
+    public static final String TASK = "Task";
+    public static final String TASKS = "Tasks";
+    public static final String TASK_LIST = "Task List";
+}
