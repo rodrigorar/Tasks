a199cd551973e7a1e3e6a04b2fa4d66de31808b8
rodrigorar <rodrigoramos.rosa2@gmail.com>
2017-08-19 00:34:47 +0100

    Closes #12

---
+++ package.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+build=tasks-0.1
+
+function build_directories() {
+	echo "Creating directory structure ..."
+	mkdir $build
+	mkdir $build/resources
+}
+
+function copy_resources() {
+	echo "Copying resources ..."
+	cp LICENSE $build/
+	cp README.md $build/
+	cp src/main/resources/install.sh $build/
+	cp src/main/resources/tasks.png $build/resources/
+	cp src/main/resources/tasks.desktop $build/resources/
+	cp src/main/resources/tasks.sh $build/resources/
+	cp src/main/resources/base_data_file.xml $build/resources/
+}
+
+function create_jar() {
+	mvn clean
+	mvn compile
+	mvn package
+	cp target/$build.jar $build
+	mvn clean
+}
+
+function package() {
+	tar -cf $build.tar.gz $build
+	rm --recursive --force $build
+}
+
+build_directories
+copy_resources
+create_jar
+package
+echo "Package Finished."
